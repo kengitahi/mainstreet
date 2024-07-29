b42c259f743b4712db69967144eb89c0115f55e6
Kennedy Gitahi <kengitahi@hotmail.com>
2024-07-30 01:34:25 +0300

    fixes

--- components/layout/Nav.js
+++ components/layout/Nav.js
@@ -10,7 +10,7 @@ const Nav = () => {
     const pathname = usePathname()
 
     return (
-        <nav className="fixed top-0 z-20 w-full bg-white border-b border-gray-200 shadow-lg dark:bg-gray-900 start-0 dark:border-gray-600 font-lexend">
+        <nav className="fixed top-0 z-20 w-full border-b border-gray-200 shadow-lg bg-white/70 dark:bg-gray-900 start-0 dark:border-gray-600 font-lexend backdrop-blur-3xl">
             <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
                 <a href="https://mainstreetdigital.netlify.app/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <Image src={Logo} alt="MainStreet Digital Logo" priority height={44} />
@@ -28,7 +28,7 @@ const Nav = () => {
                     </button>
                 </div>
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                    <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <a href="/" className={`link ${pathname === '/' ? 'block px-3 py-2 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' : 'block px-3 py-2 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'}`}>Home</a>
                         </li>
