496ba388e6f12dfb0f88eaaaf031003c31cd6c92
Kennedy Gitahi <kengitahi@hotmail.com>
2024-08-01 16:10:20 +0300

    Cosmetics

--- components/buttons/PrimaryBtnLink.js
+++ components/buttons/PrimaryBtnLink.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PrimaryBtnLink = ({ children, text, href, size, classes }) => {
 
-    let btnClasses = 'flex gap-2 text-base font-medium text-center text-white bg-blue-700 rounded-lg max-w-fit font-lexend hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ';
+    let btnClasses = 'flex gap-2 text-base font-medium text-center text-white bg-[#232BB1] rounded-lg max-w-fit font-lexend hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ';
 
     btnClasses += classes;
 
