dafee2e7e00d2531bb8865f528fbc0edf6baead6
Kennedy Gitahi <kengitahi@hotmail.com>
2025-06-22 13:41:36 +0300

    Before prod build

--- components/ui/ContactForm.jsx
+++ components/ui/ContactForm.jsx
@@ -24,7 +24,6 @@ export default function ContactForm() {
 	});
 
 	const handleFormSubmit = async (event) => {
-		console.log(formData);
 		event.preventDefault();
 		try {
 			setStatus('pending');
